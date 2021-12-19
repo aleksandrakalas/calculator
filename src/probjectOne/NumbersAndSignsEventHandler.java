d58ff2da65780705d2aa93f42f453901220fe08f
aleksandrakalas <aleksandra.kalas@mail.utoronto.ca>
2021-12-19 18:19:06 -0500

    Adding event handlers for buttons

---
+++ src/probjectOne/NumbersAndSignsEventHandler.java
@@ -0,0 +1,14 @@
+package probjectOne;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class NumbersAndSignsEventHandler implements EventHandler<ActionEvent>{
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/probjectOne/UserInterface.java
+++ src/probjectOne/UserInterface.java
@@ -31,11 +31,23 @@ public class UserInterface extends Application {
 		text.setText("Follow the given instructions to use the calculator:");
 		text.setFont(Font.font ("Times New Roman", 15));
 		text.setFill(Color.RED);
+		
 		VBox root = new VBox(); // LAYOUT
 		
 		root.setPadding(new Insets(10));
 		
 		// Adding buttons:
+		Button num1 = createButton("1");
+		Button num2 = createButton("2");
+		Button num3 = createButton("3");
+		Button num4 = createButton("4");
+		Button num5 = createButton("5");
+		Button num6 = createButton("6");
+		Button num7 = createButton("7");
+		Button num8 = createButton("8");
+		Button num9 = createButton("9");
+		Button num0 = createButton("0");
+		
 		Button equalB = createButton("=");
 		Button addB = createButton("+");
 		Button subtractB = createButton("-");
@@ -48,6 +60,7 @@ public class UserInterface extends Application {
 		
 		GridPane gp = new GridPane(); // Aligns horizontally
 		gp.addRow(0, addB, subtractB, divideB, multiplyB, equalB);
+		gp.addRow(1, num0, num1, num2, num3, num4, num5, num6, num7, num8, num9);
 		
 		// Adding the items to the screen
 		root.getChildren().addAll(text, gp);
